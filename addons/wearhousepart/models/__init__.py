1f4f73f9170df6dd92a38e6ab6544098d09e8eeb
KinPa01 <autodonut.2548@gmail.com>
2026-05-14 17:16:36 +0700

    del

--- addons/wearhousepart/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import spare_category
-from . import spare_warehouse
-from . import spare_location
-from . import spare_part
-from . import spare_stock_move
-from . import spare_motorcycle
-from . import spare_purchase_order
-from . import spare_request
